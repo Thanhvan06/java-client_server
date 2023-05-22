efa2f28fded494e9659d0aed78cfefb20d93d5d3
Thanhvan06 <101513252+Thanhvan06@users.noreply.github.com>
2023-05-22 10:00:09 +0700

    java
    Java swing combined with jdbc , client server connection based on mvc model.

---
+++ Project_Final/src/test/run.java
@@ -0,0 +1,11 @@
+package test;
+
+import view.view_login;
+import view.view_main;
+
+public class run {
+	public static void main(String[] args) {
+		new view_login().setVisible(true);
+//		new view_main().setVisible(true);
+	}
+}
